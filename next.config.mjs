c555ad3c26468d35929a70cdca42e15ec4f62939
B3hnAM <br.1515@yahoo.com>
2025-12-08 02:23:44 +0330

    update
    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.c2.liara.space',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
     headers: async () => {
         return [
             {
